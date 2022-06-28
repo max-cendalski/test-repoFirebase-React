5cd8382faefc54d37feff7e2e9a042a9dadf05c9
Korneliu <olbiton@gmail.com>
2022-06-28 16:51:13 -0700

    fixed firebase context

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -1,21 +1,23 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import app from 'firebase/compat/app';
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyA0MSi1LtnYKGmzZWoJ014C0a7wxQeg6nw",
-  authDomain: "test-firebasereact.firebaseapp.com",
-  projectId: "test-firebasereact",
-  storageBucket: "test-firebasereact.appspot.com",
-  messagingSenderId: "104330345851",
-  appId: "1:104330345851:web:c505740532e83f766c91de",
-  measurementId: "G-XVPS8QY4KG"
+
+const config = {
+  apiKey: process.env.REACT_API_KEY,
+  authDomain:process.env.REACT_AUTH_DOMAIN,
+  projectId: process.env.EACTP_PROJECT_ID,
+  storageBucket: process.env.REACT_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+
+
+class Firebase {
+  constructor() {
+    app.initializeApp(config)
+  }
+}
+
+export default Firebase;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+
 import App from './components/App'
 import reportWebVitals from './reportWebVitals';
+
+import Firebase, {FirebaseContext} from './components/Firebase';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <FirebaseContext.Provider value = {new Firebase()}>
+      <App />
+    </FirebaseContext.Provider>
   </React.StrictMode>
 );
 
