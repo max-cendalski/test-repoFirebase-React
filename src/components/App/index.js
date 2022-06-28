e76e587bc4ddd9c46afb05485b0c6617898c9a2e
Korneliu <olbiton@gmail.com>
2022-06-27 19:14:58 -0700

    fixed Routes in App component

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,15 +1,37 @@
 import React from 'react'
-import {BrowserRouter as Router} from 'react-router-dom'
+import * as ROUTES from '../../constants/routes'
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+import LandingPage from '../Landing';
+import SignUpPage from '../SignUp'
+import SingInPage from '../SignIn';
+import Navigation from '../Navigation';
+import PasswordForgetPage from '../PasswordForget';
+import HomePage from '../Home';
+import AccountPage from '../Account';
+import AdminPage from '../Admin';
 
-import Navigation from '../Navigation'
+
+
 
 const App = () => {
 
   return (
     <Router>
-      <Navigation />
-    </Router>
+      <article>
+        <Navigation />
+        <hr />
+        <Routes>
+          <Route exact path={ROUTES.LANDING} element={<LandingPage />} />
+          <Route exact path={ROUTES.SIGN_UP} element={<SignUpPage />} />
+          <Route exact path={ROUTES.SIGN_IN} element={<SingInPage />} />
+          <Route exact path={ROUTES.PASSWORD_FORGET} element={<PasswordForgetPage />} />
+          <Route exact path={ROUTES.ACCOUNT} element={<AccountPage />} />
+          <Route exact path={ROUTES.ADMIN} element={<AdminPage />} />
+          <Route exact path={ROUTES.HOME} element={<HomePage />} />
+        </Routes>
 
+      </article>
+    </Router>
   )
 }
 
