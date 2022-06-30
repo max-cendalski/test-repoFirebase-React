abbb5f9213bc5a5e4336ab96941100674ac2d8d2
Korneliu <olbiton@gmail.com>
2022-06-29 19:36:37 -0700

    added HOC to context.js

--- src/components/Firebase/context.js
+++ src/components/Firebase/context.js
@@ -2,4 +2,10 @@ import React from 'react';
 
 const FirebaseContext = React.createContext(null);
 
+export const withFirebase = Component => (
+  <FirebaseContext.Consumer>
+    {firebase => <Component {...props} firebase={firebase} />}
+  </FirebaseContext.Consumer>
+)
+
 export default FirebaseContext;
