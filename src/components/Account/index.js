04831989dd70717c40a95636aa591dea09228552
Korneliu <olbiton@gmail.com>
2022-07-01 19:52:05 -0700

    added logout button

--- src/components/Account/index.js
+++ src/components/Account/index.js
@@ -1,14 +1,26 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import { UserAuth } from '../Firebase/context';
 
 const Account = () => {
   const {user, logout} = UserAuth()
-  console.log('userv',user)
+  const navigate = useNavigate()
+
+  const handleLogout = async () => {
+    try {
+      await logout()
+      navigate('/')
+      console.log('You are logged out')
+    } catch(e) {
+      console.log(e.message)
+    }
+  }
+
   return (
     <div>
       <h1>Account</h1>
       <p>User Email: {user && user.email}</p>
-      <button>Logout</button>
+      <button onClick={handleLogout}>Logout</button>
   </div>
   )
 }
