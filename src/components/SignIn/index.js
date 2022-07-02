1c332b925cb8f06819a8908203d764bcffb94226
Korneliu <olbiton@gmail.com>
2022-07-01 20:18:36 -0700

    added signin function

--- src/components/SignIn/index.js
+++ src/components/SignIn/index.js
@@ -1,52 +1,54 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
+import {Link, useNavigate} from 'react-router-dom';
+import { useState } from 'react';
+import { UserAuth } from '../Firebase/context';
 
 const SignIn = () => {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [error, setError] = useState('')
+  const {signIn} = UserAuth()
+  const navigate = useNavigate()
 
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    try {
+      await signIn(email,password)
+      navigate('/account')
+    } catch (e) {
+      setError(e.message)
+      console.log('ERROR',error)
+    }
+  }
+
+  const isInvalid = email === '' || password === '';
   return (
     <article>
       <h1>Sign in to your account</h1>
       <p>Don't have an account yet? <Link to="/signup">SignUp</Link></p>
+      <form onSubmit={handleSubmit} className="signup-form">
+        <input
+          name='email'
+          value={email}
+          onChange={(e)=>setEmail(e.target.value)}
+          type='text'
+          placeholder='Email Address'
+        />
+          <input
+          name='password'
+          value={password}
+          onChange={(e)=>setPassword(e.target.value)}
+          type='password'
+          placeholder='Password'
+        />
+        <button
+        type='submit'
+        disabled={isInvalid}
+        >Sign In</button>
+        {error && <p>{error.message}</p>}
+      </form>
 
     </article>
   )
 }
 
 export default SignIn;
-
-/*
- <form className="signup-form">
-        <input
-          name='username'
-          value={username}
-          onChange={this.onChange}
-          type='text'
-          placeholder='Full Name'
-        />
-        <input
-          name='email'
-          value={email}
-          onChange={this.onChange}
-          type='text'
-          placeholder='Email Address'
-        />
-          <input
-          name='passwordOne'
-          value={passwordOne}
-          onChange={this.onChange}
-          type='password'
-          placeholder='Password'
-        />
-          <input
-          name='passwordTwo'
-          value={passwordTwo}
-          onChange={this.onChange}
-          type='password'
-          placeholder='Confirm Password'
-        />
-        <button
-        type='submit'
-        disabled={isInvalid}
-        >Sign Up</button>
-        {error && <p>{error.message}</p>}
-      </form> */
