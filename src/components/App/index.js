4bd58fd3959eef097a305543ad849af1a8d17b91
Korneliu <olbiton@gmail.com>
2022-07-03 15:10:35 -0700

    removed unnecessary code

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -48,11 +48,3 @@ const App = () => {
 }
 
 export default App
-
-export const LANDING = '/';
-export const SIGN_UP = '/signup';
-export const SIGN_IN = '/signin';
-export const HOME = '/home';
-export const ACCOUNT = '/account';
-export const ADMIN = '/admin';
-export const PASSWORD_FORGET = '/pw-forget';
