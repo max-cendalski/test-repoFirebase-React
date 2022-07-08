b83f8127fe45d0f6709baafa0773fc13295ed366
Korneliu <olbiton@gmail.com>
2022-07-07 20:27:05 -0700

    changed setInterval to setTimeOut in Loading component

--- src/components/Loading/Loading.js
+++ src/components/Loading/Loading.js
@@ -3,10 +3,11 @@ import { useState, useEffect } from "react"
 
 const Loading = () => {
   const [loadingClass, setLoadingClass] = useState('loading-element')
+
   useEffect(()=> {
-    setInterval(()=> {
+    setTimeout(()=> {
       setLoadingClass('loading-element-two')
-    })
+    },100)
   },[])
   return (
     <div className={loadingClass}>
