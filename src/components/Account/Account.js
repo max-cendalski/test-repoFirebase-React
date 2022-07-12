d69c6a760a913282d2b5cdffcc13b4ded2cd1820
Korneliu <olbiton@gmail.com>
2022-07-11 18:35:04 -0700

    added storage bucket URL

--- src/components/Account/Account.js
+++ src/components/Account/Account.js
@@ -2,10 +2,18 @@ import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import { UserAuth } from '../Firebase/context';
 import Todos from '../Todos/Todos'
+import { getStorage, ref } from 'firebase/storage';
 
 const Account = () => {
+ const storage = getStorage()
+  const storageRef = ref(storage)
+
+
   const {user, logout} = UserAuth()
   const navigate = useNavigate()
+
+  const picRef = ref(storageRef, 'George11.jpg')
+
 
   const handleLogout = async () => {
     try {
@@ -21,6 +29,7 @@ const Account = () => {
     <article className='account-container'>
       <h1>Account</h1>
       <p>User Email: {user && user.email}</p>
+      <img src={picRef} alt="George"></img>
       <button onClick={handleLogout}>Logout</button>
       <hr />
       <Todos />
--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -2,6 +2,7 @@
 import {initializeApp} from "firebase/app"
 import {getAuth} from "firebase/auth"
 import {getDatabase} from "firebase/database"
+import {getStorage} from "firebase/storage"
 
 
 const firebaseConfig = {
@@ -17,6 +18,7 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig)
 
+export const storage = getStorage(app);
 export const database = getDatabase(app)
 export const auth = getAuth(app)
 export default app
