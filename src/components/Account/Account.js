185505cdfb52821affe8d75a6a959dc1ef2a272a
Korneliu <olbiton@gmail.com>
2022-07-13 17:27:55 -0700

    installed uuid

--- src/components/Account/Account.js
+++ src/components/Account/Account.js
@@ -3,20 +3,27 @@ import { useNavigate} from 'react-router-dom';
 import { useState,useEffect } from 'react';
 import { UserAuth } from '../Firebase/context';
 import Todos from '../Todos/Todos'
-import { getStorage, ref, getDownloadURL, listAll, list } from 'firebase/storage';
+import { getStorage, ref, getDownloadURL, listAll, list, uploadBytes } from 'firebase/storage';
 import Loading from '../Loading/Loading';
 
 const Account = () => {
    const {user, logout} = UserAuth()
    const [imagesList, setImagesList] = useState([])
    const [image, setImage] = useState('')
+  const  [imageUpload, setImageUpload] = useState(null)
+
+
+   const fileInputRef = React.createRef()
 
    const navigate = useNavigate()
 
    const storage = getStorage()
-   const imagesRef = ref(storage, 'images/')
+   const imagesRef = ref(storage, 'newfiles/'+ fileInputRef)
+
    const geImg = ref(storage,'images/George11.jpg')
-   console.log('geIm.fullPath',geImg.fullPath)
+
+
+   const newImgRef = ref(storage, 'newfiles')
 
     useEffect(()=> {
       getDownloadURL(ref(storage, geImg))
@@ -24,8 +31,27 @@ const Account = () => {
         console.log('url',url)
         setImage(url)
       })
+    },[])
 
+  const handleFileUpload = e => {
+    //e.preventDefault()
+    const formData = new FormData();
+    formData.append('testImage',fileInputRef);
+    console.log('fileInputRef',fileInputRef)
+    if (fileInputRef.current.files.length === 0) {
+      console.log('whehe')
+    } else {
+        uploadBytes(imagesRef, formData)
+        .then((snapshot) => {
+      console.log('Uploaded a file!')
     })
+
+      console.log('bingo')
+    }
+
+
+  }
+
 
 
   const handleLogout = async () => {
@@ -46,6 +72,18 @@ const Account = () => {
       <img className="image-container" src={image} alt="George"></img>
       <button onClick={handleLogout}>Logout</button>
       <hr />
+      <section>
+
+      <input
+        type="file"
+        onChange={(event) => {
+          setImageUpload(event.target.files[0]);
+        }}
+      />
+      <button onClick={uploadFile}> Upload Image</button>
+
+
+      </section>
       <Todos />
   </article>
   )
